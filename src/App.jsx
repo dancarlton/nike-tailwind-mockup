847b7fcd444bd3e609f4ca9bd12e26bba5abf5d2
Dan Carlton <danlouis.carlton@gmail.com>
2023-11-26 12:20:35 -0800

    rendered individual components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import CustomerReviews from "./components/CustomerReviews"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import PopularProducts from "./components/PopularProducts"
+import Services from "./components/Services"
+import SpecialOffer from "./components/SpecialOffer"
+import Subscribe from "./components/Subscribe"
+import SuperQuality from "./components/SuperQuality"
+
 const App = () => (
   <main className='relative'>
     Nav
-    <section className='xl:padding-1 wide:padding-r padding-b'>Hero</section>
-    <section className='padding'></section>
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+      <Hero />
+    </section>
+    <section className='padding'>
+      <PopularProducts />
+    </section>
+    <section className='padding'>
+      <SuperQuality />
+    </section>
+    <section className='padding-x py-10'>
+      <Services />
+    </section>
+    <section className='padding'>
+      <SpecialOffer />
+    </section>
+    <section className='padding bg-pale-blue'>
+      <CustomerReviews />
+    </section>
+    <section className='padding-x sm:py-32 py-16 w-full'>
+      <Subscribe />
+    </section>
+    <section className='padding bg-black padding-x padding-t pb-8'>
+      <Footer />
+    </section>
   </main>
 )
 
